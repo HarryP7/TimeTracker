68463c03cd2bc9d17201cc432408fec0097d05d4
Мартынов Григорий <grishkamartov@gmail.com>
2026-08-30 23:51:56 +0500

    Add-Migration Add_GeneralInfoTimeDay_DbTable

--- Data/Migrations/20260830184747_Add_GeneralInfoTimeDay_DbTable.Designer.cs
+++ Data/Migrations/20260830184747_Add_GeneralInfoTimeDay_DbTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TimeTracker.Data;
@@ -11,9 +12,11 @@ using TimeTracker.Data;
 namespace TimeTracker.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260830184747_Add_GeneralInfoTimeDay_DbTable")]
+    partial class Add_GeneralInfoTimeDay_DbTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20260830184747_Add_GeneralInfoTimeDay_DbTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimeTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_GeneralInfoTimeDay_DbTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "general_info_time_day",
+                columns: table => new
+                {
+                    date = table.Column<DateOnly>(type: "date", nullable: false),
+                    work_start_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    total_pause_seconds = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_general_info_time_day", x => x.date);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "general_info_time_day");
+        }
+    }
+}
