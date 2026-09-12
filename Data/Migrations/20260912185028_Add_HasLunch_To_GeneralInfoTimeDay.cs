6ffb76dfcbd8400cc91a541af37b6514246d4b63
Мартынов Григорий <grishkamartov@gmail.com>
2026-09-12 23:50:52 +0500

    Add-Migration Add_HasLunch_To_GeneralInfoTimeDay

---
+++ Data/Migrations/20260912185028_Add_HasLunch_To_GeneralInfoTimeDay.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimeTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_HasLunch_To_GeneralInfoTimeDay : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "has_lunch",
+                table: "general_info_time_day",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "has_lunch",
+                table: "general_info_time_day");
+        }
+    }
+}
